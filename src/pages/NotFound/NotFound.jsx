e4ce64553d2fca47c4aed1b57fa2b343978e5cd2
jerem p <palette.jeremie@live.fr>
2025-11-06 14:02:02 +0100

    add header and footer with 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,14 +1,21 @@
 // src/pages/NotFound/NotFound.jsx
 import { Link } from "react-router-dom";
 import "./NotFound.scss";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 export default function NotFound() {
   return (
+    <>
+    <Header />
     <main className="notfound">
       <h1 className="notfound__code">404</h1>
       <p className="notfound__text">Oups! La page que vous demandez n'existe pas.</p>
       <Link to="/" className="notfound__link">Retourner sur la page d’accueil</Link>
     </main>
+
+    <Footer />
+    </>
   );
 }
 
